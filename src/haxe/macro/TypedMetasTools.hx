06f62ad7980d71712cfb6a77ae345fd15f7a6580
skial <skial.bainn@gmail.com>
2023-05-09 11:22:02 +0100

    Update to haxe 4.3.1 and latest meta.json content. Rename build.hxml to build.tests.hxml Remove tink_* libraries Metas.json define is now metas.path which is required to generate haxe files. TypedMetas.hx now favouring `@:op(a())` operators instead of TypedMetasTools.hx usings which are now deprecated.

--- src/haxe/macro/TypedMetasTools.hx
+++ src/haxe/macro/TypedMetasTools.hx
@@ -4,24 +4,28 @@ import haxe.macro.Expr;
 import haxe.macro.Context;
 
 class Voids {
+    @:deprecated("Call TypedMeta directly. Metadata().")
     public static inline function asMetadataEntry<T>(tm:TypedMetas<Void, T>, ?pos:Position):MetadataEntry {
         return { name: tm, params: [], pos: pos == null ? Context.currentPos() : pos };
     }
 }
 
 class Expression {
+    @:deprecated("Call TypedMeta directly. Metadata(macro expr).")
     public static inline function asMetadataEntry<A:Expr, T>(tm:TypedMetas<A, T>, value:A, ?pos:Position):MetadataEntry {
         return { name: tm, params: [value], pos: pos == null ? Context.currentPos() : pos };
     }
 }
 
 class Expressions {
+    @:deprecated("Call TypedMeta directly. Metadata([macro expr0, macro exprN]).")
     public static inline function asMetadataEntry<A:Array<Expr>, T>(tm:TypedMetas<A, T>, value:A, ?pos:Position):MetadataEntry {
         return { name: tm, params: value, pos: pos == null ? Context.currentPos() : pos };
     }
 }
 
 class InlinedVoids {
+    @:deprecated("Call TypedMeta directly. Metadata().")
     public static function inlined<T:Expr>(tm:TypedMetas<Void, T>, expr:T, ?pos:Position):Expr {
         if (pos == null) pos = Context.currentPos();
         return { expr:EMeta({name:tm, params:[], pos:pos}, expr), pos: pos };
@@ -29,6 +33,7 @@ class InlinedVoids {
 }
 
 class InlinedSingleAny {
+    @:deprecated("Call TypedMeta directly. Metadata(macro expr).")
     public static function inlined<T>(tm:TypedMetas<Expr, T>, param:Expr, expr:Expr, ?pos:Position):Expr {
         if (pos == null) pos = Context.currentPos();
         return { expr:EMeta({name:tm, params:[param], pos:pos}, expr), pos: pos };
@@ -36,8 +41,16 @@ class InlinedSingleAny {
 }
 
 class InlinedArrayAny {
+    @:deprecated("Call TypedMeta directly. Metadata([macro expr0, macro exprN]).")
     public static function inlined(tm:TypedMetas<Array<Expr>, Any>, params:Array<Expr>, expr:Expr, ?pos:Position):Expr {
         if (pos == null) pos = Context.currentPos();
         return { expr:EMeta({name:tm, params:params, pos:pos}, expr), pos: pos };
+    }
+}
+
+class Empty {
+    @:deprecated("Call TypedMeta directly. Metadata().")
+    public static inline function asMetadataEntry<T>(tm:TypedMetas<Any, T>, ?pos:Position):MetadataEntry {
+        return { name: tm, params: [], pos: pos == null ? Context.currentPos() : pos };
     }
 }
--- test/Entry.hx
+++ test/Entry.hx
@@ -16,6 +16,16 @@ class Entry {
         var expr = NullSafety.inlined(macro Strict, macro a != null ? 1 : 2);
         trace( expr );
         trace( printer.printExpr(expr) );
+        var td = (macro class Test {});
+        td.meta.push(Build([macro path.to.Build.call()], td.pos));
+        trace( printer.printTypeDefinition(td) );
+        trace( printer.printMetadata( Build() ) );
+        /*trace( printer.printMetadata( Build([macro path.to.Build.call()]) ) );*/
+        trace( printer.printMetadata( Build(macro path.to.Build.call()) ) );// via static @:op(a()) with extra `from String`
+        trace( printer.printMetadata( Build([macro 0, macro 'foo']) ) );
+        trace( printer.printMetadata( Build('hello world') ) );
+        trace( printer.printMetadata( Build(['hello', 'world']) ) );
+        trace( printer.printMetadata( Build([.3,.4,.5]) ) );
     }
 
 }
